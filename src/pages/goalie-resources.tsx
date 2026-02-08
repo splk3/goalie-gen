c69b53d5df9fbca06dea1cc50fab63e01578a056
Patrick Boyle <parickjboyle@gmail.com>
2026-02-07 22:07:43 -0500

    UI updates

--- src/pages/goalie-resources.tsx
+++ src/pages/goalie-resources.tsx
@@ -15,11 +15,6 @@ export default function GoalieResources() {
             <Link to="/">
               <Logo variant="full" format="png" className="w-32 md:w-48 lg:w-64" />
             </Link>
-            <div className="hidden md:block flex-1 mx-4">
-              <p className="text-lg md:text-xl lg:text-2xl font-semibold text-center">
-                Goalie Resources
-              </p>
-            </div>
             <DarkModeToggle />
           </div>
         </div>
