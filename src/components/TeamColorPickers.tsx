272c3717a4d3a5670a1e7ef05fe37041b17908d3
Patrick Boyle <patrickjboyle@gmail.com>
2026-06-27 12:11:22 -0400

    fix formatting for mobile custom color pickers

--- src/components/TeamColorPickers.tsx
+++ src/components/TeamColorPickers.tsx
@@ -79,7 +79,7 @@ function ColorPickerControl({
           <input
             id={`${idPrefix}-color`}
             type="color"
-            value={value}
+            value={value.toLowerCase()}
             onChange={handleColorPickerChange}
             disabled={disabled}
             className="h-10 w-16 cursor-pointer rounded border border-gray-300 bg-white p-1 disabled:cursor-not-allowed"
