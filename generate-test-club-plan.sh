cdf03bb7e61e347c69176d27cfd4ed714e1e4e3f
Patrick Boyle <patrickjboyle@gmail.com>
2026-07-18 13:28:46 -0400

    fix typo in test plan generator

--- generate-test-club-plan.sh
+++ generate-test-club-plan.sh
@@ -3,7 +3,7 @@
 npx tsx generate-test-club-plan.ts \
 	--name "Brandywine Outlaws" \
 	--website "https://www.facebook.com/brandywineoutlaws/" \
-	--motto "The most fun you'll ever have playing summer hockey\!" \
+	--motto "The most fun you'll ever have playing summer hockey!" \
 	--logo static/images/test/logos/outlaws.jpeg \
 	--primary "#081f8a" \
 	--secondary "#7b7e8c" \
